218a1e4eb147c0d6c0b4274058c182b6381825f0
shubham-yadav-bluepineapple <shubham.yadav@bluepineapple.io>
2023-06-16 15:21:09 +0530

    added functions.

---
+++ advanced.js
@@ -0,0 +1,10 @@
+const power = (num1, ) => {
+    return Math.pow(num1, num2);
+  };
+  
+  const sqrt = (num1) => {
+    return Math.sqrt(num1);
+  };
+  
+  export { power, sqrt };
+  
---
+++ index.js
@@ -0,0 +1,18 @@
+import { power, sqrt } from "./advanced";
+import { add, subtract } from "./math";
+
+const addResult = add(10, 20);
+
+console.log("Addition: " + addResult);
+
+const subtractResult = subtract(20, 10);
+
+console.log("Subtraction: " + subtractResult);
+
+const powerResult = power(5, 2);
+
+console.log("Power: " + powerResult);
+
+const sqrtResult = sqrt(25);
+
+console.log("Square Root: " + sqrtResult);
